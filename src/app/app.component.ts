9ba9598ebf26505a793051dd28cdd1d219af8c73
Mauricio Mendes <mauricio.jmmendes@gmail.com>
2018-04-07 17:36:33 -0300

    Added Google authentication feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'VNT-BOOTCAMP';
+
+  constructor(private authService: AuthService) {
+  }
+
+  login() {
+    this.authService.login();
+  }
+
+  logout() {
+    this.authService.logout();
+  }
 }
--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@angular/core';
 import { CanActivate } from '@angular/router';
+import { AngularFireAuth } from 'angularfire2/auth';
+
+import * as firebase from 'firebase/app';
+import { FirebaseApp } from 'angularfire2';
 
 @Injectable()
 export class AuthService implements CanActivate {
@@ -8,6 +12,15 @@ export class AuthService implements CanActivate {
     console.log("CanActivate was called");
     return true;
   }
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) {
+  }
+
+  login() {
+    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
+
+  logout() {
+    this.afAuth.auth.signOut();
+  }
 
 }
